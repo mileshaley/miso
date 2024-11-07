ccd81adf054af247dfd8b8b3a849f9de3eaa4eaa
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-11-06 19:30:22 -0800

    added remove functionality for variadic packs
    can be used inside of pack or externally (still have to put the args in a pack and receive them back as a pack). implementation is in miso::detail namespace

--- include/types.h
+++ include/types.h
@@ -7,43 +7,79 @@
 #pragma once
 
 #include <utility> // std::tuple
+#include <type_traits>
 
 namespace miso {
+	// Represents a NULL type
+	// For example: The result of geting first argument of pack<> (empty)
+	struct no_type {
+		using type = no_type;
+	};
+
 	// Forward declare pack
 	template<typename... Ts>
 	struct pack;
 
-	//namespace detail {
-	//	// Separating this logic allows us to 
-	//	template<typename First, typename...>
-	//	using first_type_arg_get_aux = First;
-	//	template<typename, typename... Rest>
-	//	using first_type_arg_remove_aux = pack<Rest...>;
-	//
-	//} // namespace detail
-
 	// Primary template
+	// Also handles 0 Ts case
 	template<typename... Ts>
 	struct first_arg {
 		using get = void; // Doesn't exist
 		using remove = pack<>;
 	};
 
+	// Isolates first type argument for getting or removing
 	template<typename First, typename... Rest>
 	struct first_arg<First, Rest...> {       
 		using get = First;
 		using remove = pack<Rest...>;
 	};
 
-	// miso::pack is a type that can be used like a container of type objects
+	namespace detail {
+		// Primary template
+		template<class NewPack, class OriginalPack, typename... Rest>
+		struct remove_ts_aux;
+
+		// Handles 0 arguments left (base case)
+		template<typename... NPAs, typename... Rest>
+		struct remove_ts_aux<pack<NPAs...>, pack<>, Rest...> {
+			using type = pack<NPAs...>;
+		};
+
+		template<typename... NPAs, typename... OPAs, typename... Rest>
+		struct remove_ts_aux<pack<NPAs...>, pack<OPAs...>, Rest...> {
+			using first_og = typename first_arg<OPAs...>::get;
+			using rest_og  = typename first_arg<OPAs...>::remove;
+			using type     = std::conditional_t<
+				(std::_Is_any_of_v<first_og, Rest...>),
+				typename remove_ts_aux<pack<NPAs...			 >, 
+									   rest_og, Rest...>::type,
+				typename remove_ts_aux<pack<NPAs..., first_og>, 
+									   rest_og, Rest...>::type
+			>;
+		};
+
+	} // namespace detail
+	
+	// Helper to remove all types RemoveTs from a pack
+	template<class Pack, typename... RemoveTs>
+	using remove_types_t = detail::remove_ts_aux<
+		pack<>, Pack, RemoveTs...>::type;
+
+	// miso::pack is a type that can be used like a container of type objects.
+	// pack is used all over this file as a "return type" of multiple types
+	//   or as an additional template parameter pack
 	template<typename... Ts>
 	struct pack {
 		using as_tuple = std::tuple<Ts...>;
 
 		template<typename... NewTs>
 		using add = pack<Ts..., NewTs...>;
+
+		using first = first_arg<Ts...>;
+
+		template<typename... RemoveTs>
+		using remove = remove_types_t<pack<Ts...>, RemoveTs...>;
 	};
-
-
 
 } // namespace miso
