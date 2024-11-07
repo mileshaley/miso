2dc7f936a0475b31589f8ee22807bbdcc69fdfe1
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-11-06 18:49:56 -0800

    added accessor class template first_arg
    first_arg allows you to either get the first type in a variadic template type parameter pack or remove it (gives you a pack with the rest of the args)

--- include/types.h
+++ include/types.h
@@ -9,12 +9,41 @@
 #include <utility> // std::tuple
 
 namespace miso {
+	// Forward declare pack
+	template<typename... Ts>
+	struct pack;
+
+	//namespace detail {
+	//	// Separating this logic allows us to 
+	//	template<typename First, typename...>
+	//	using first_type_arg_get_aux = First;
+	//	template<typename, typename... Rest>
+	//	using first_type_arg_remove_aux = pack<Rest...>;
+	//
+	//} // namespace detail
+
+	// Primary template
+	template<typename... Ts>
+	struct first_arg {
+		using get = void; // Doesn't exist
+		using remove = pack<>;
+	};
+
+	template<typename First, typename... Rest>
+	struct first_arg<First, Rest...> {       
+		using get = First;
+		using remove = pack<Rest...>;
+	};
+
+	// miso::pack is a type that can be used like a container of type objects
 	template<typename... Ts>
 	struct pack {
-		using as_tuple_t = std::tuple<Ts...>;
+		using as_tuple = std::tuple<Ts...>;
 
 		template<typename... NewTs>
 		using add = pack<Ts..., NewTs...>;
 	};
+
+
 
 } // namespace miso
--- test/types.test.h
+++ test/types.test.h
@@ -2,9 +2,6 @@
 
 namespace miso::testing {
 	void test_types() {
-
-		// Forward declare pack
-		template<typename... Ts>
-		struct pack;
+		
 	}
 } // namespace miso::testing
