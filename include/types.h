f2d00cf13e947f465aadb663cb8b63c9442790b8
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-11-06 18:25:31 -0800

    added basic class template pack
    pack serves as a way to pass a template type parameter pack as a single type. It will be able to be used sort of like a container class with methods for operating on data but for types.

--- include/types.h
+++ include/types.h
@@ -6,6 +6,15 @@
 
 #pragma once
 
+#include <utility> // std::tuple
+
 namespace miso {
+	template<typename... Ts>
+	struct pack {
+		using as_tuple_t = std::tuple<Ts...>;
+
+		template<typename... NewTs>
+		using add = pack<Ts..., NewTs...>;
+	};
 
 } // namespace miso
--- test/types.test.h
+++ test/types.test.h
@@ -3,5 +3,8 @@
 namespace miso::testing {
 	void test_types() {
 
+		// Forward declare pack
+		template<typename... Ts>
+		struct pack;
 	}
 } // namespace miso::testing
