b88edf1decb2fb281fd4770b649889f418af3a5f
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2024-11-07 21:29:25 -0800

    added flatten_t and flatten_into_tuple_t
    helper aliases for normalizing a lot of types that are possibly deeply nested in tuples into a single depth 1 container of types

--- include/types.h
+++ include/types.h
@@ -10,7 +10,7 @@
 #include <type_traits>
 
 namespace miso {
-	// Represents a NULL type
+	// Represents a nonexistent/null type
 	// For example: The result of geting first argument of pack<> (empty)
 	struct no_type {
 		using type = no_type;
@@ -50,6 +50,8 @@ namespace miso {
 		struct remove_ts_aux<pack<NPAs...>, pack<OPAs...>, Rest...> {
 			using first_og = typename first_arg<OPAs...>::get;
 			using rest_og  = typename first_arg<OPAs...>::remove;
+			// If the type in the original pack is the same as any remove type,
+			// don't add it to the pack, otherwise add it. Then recurse
 			using type     = std::conditional_t<
 				(std::_Is_any_of_v<first_og, Rest...>),
 				typename remove_ts_aux<pack<NPAs...			 >, 
@@ -66,9 +68,9 @@ namespace miso {
 	using remove_types_t = detail::remove_ts_aux<
 		pack<>, Pack, RemoveTs...>::type;
 
-	// miso::pack is a type that can be used like a container of type objects.
+	// Type that can be used like a container of type objects.
 	// pack is used all over this file as a "return type" of multiple types
-	//   or as an additional template parameter pack
+	//   or as an additional variadic template parameter pack
 	template<typename... Ts>
 	struct pack {
 		using as_tuple = std::tuple<Ts...>;
@@ -81,5 +83,50 @@ namespace miso {
 		template<typename... RemoveTs>
 		using remove = remove_types_t<pack<Ts...>, RemoveTs...>;
 	};
+
+	namespace detail {
+		// Primary template
+		template<class Pack, typename First, typename... Rest>
+		struct flatten;
+
+		// Primary template
+		template<class Pack, typename... Rest>
+		struct reflatten;
+
+		// Last arg is non-tuple (guaranteed endpoint)
+		template<class Pack, typename First>
+		struct flatten<Pack, First> {
+			using type = typename Pack::template add<First>;
+		};
+		// Last arg is tuple (requires reflatten)
+		template<class Pack, typename... FirstArgs>
+		struct flatten<Pack, std::tuple<FirstArgs...>> {
+			using type = typename reflatten<typename Pack::template add<FirstArgs...>>::type;
+		};
+		// Arg is non-tuple
+		template<class Pack, typename First, typename... Rest>
+		struct flatten {
+			using type = typename flatten<typename Pack::template add<First>, Rest...>::type;
+		};
+		// Arg is tuple (requires reflatten)
+		template<class Pack, typename... FirstArgs, typename... Rest>
+		struct flatten<Pack, std::tuple<FirstArgs...>, Rest...> {
+			using type = typename reflatten<typename Pack::template add<FirstArgs...>, Rest...>::type;
+		};
+		// Reflatten all args to get all nested tuples at any depth
+		template<typename... PackArgs, typename... Rest>
+		struct reflatten<pack<PackArgs...>, Rest...> {
+			using type = typename flatten<pack<>, PackArgs..., Rest...>::type;
+		};
+	} // namespace detail
+
+	// Helper template that combines any number of
+	// types or tuples (nested to any depth) into a pack
+	template<typename... Args>
+	using flatten_t = typename detail::flatten<pack<>, Args...>::type;
+	// Helper template that combines any number of
+	// types or tuples (nested to any depth) into another tuple
+	template<typename... Args>
+	using flatten_into_tuple_t = typename flatten_t<Args...>::as_tuple;
 
 } // namespace miso
